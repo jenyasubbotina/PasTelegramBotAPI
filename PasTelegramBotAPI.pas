7a864a1ad12bd98e29f91a6d5c84180cea532ccc
Subbotina Eugenia <63358524+jenyasubbotina@users.noreply.github.com>
2020-06-07 20:18:35 +0500

    Add files via upload

--- PasTelegramBotAPI.pas
+++ PasTelegramBotAPI.pas
@@ -13,13 +13,14 @@ type
     private 
         TBotToken: string;
     public 
-        constructor Create(Token: string);
+        constructor Create(token: string);
         function GetUpdates(offset: integer := 0; limit: integer := 10; timeout: integer := 0): string;
+        function SendMessage(chatID: string; message: string): string;
     end;
 
 implementation
 
-constructor TBot.Create(Token: string);
+constructor TBot.Create(token: string);
 begin
     TBotToken := Token;
 end;
@@ -27,21 +28,38 @@ end;
 function TBot.GetUpdates(offset: integer; limit: integer; timeout: integer): string;
 var
     request: HttpWebRequest;
-    response: WebResponse;
-    datastream: Stream;
     readstream: StreamReader;
     res: string;
 begin
-    ServicePointManager.SecurityProtocol := SecurityProtocolType.Tls12;
+    ServicePointManager.SecurityProtocol := SecurityProtocolType.Tls12 or SecurityProtocolType.Tls11 or SecurityProtocolType.Tls;
     request := HttpWebRequest(WebRequest.Create(telegramUrl + TBotToken + '/getUpdates'));
     request.Method := 'GET';
-    response := request.GetResponse;
-    datastream := response.GetResponseStream;
-    readstream := new StreamReader(response.GetResponseStream, System.Text.Encoding.UTF8);
+    readstream := new StreamReader(request.GetResponse.GetResponseStream, System.Text.Encoding.UTF8);
     res := readstream.ReadToEnd;
     readstream.Close;
-    response.Close; 
     Result := res;
+end;
+
+function TBot.SendMessage(chatID: string; message: string): string;
+var
+    request: HttpWebRequest;
+begin
+    request := HttpWebRequest(WebRequest.Create(telegramUrl + TBotToken + '/sendMessage?chat_id=' + chatID + '&text=' + message));
+    request.Method := 'POST';
+    var byteArray := Encoding.UTF8.GetBytes(message);
+    request.ContentType := 'application/x-www-form-urlencoded';
+    request.ContentLength := byteArray.Length;
+    var dataStream := request.GetRequestStream();
+    dataStream.Write(byteArray, 0, byteArray.Length);
+    dataStream.Close();
+    Writeln(telegramUrl + TBotToken + '/sendMessage?chat_id=' + chatID + '&' + message);
+    var response: WebResponse := request.GetResponse();
+    writeln(((HttpWebResponse)(response)).StatusDescription);
+    dataStream := response.GetResponseStream();
+    var reader: StreamReader := new StreamReader(dataStream);
+    var responseFromServer := reader.ReadToEnd();
+    writeln(responsefromserver);
+    response.Close();
 end;
 
 end.

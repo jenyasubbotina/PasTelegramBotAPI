33ba0303f5226d2df1675e13681dc94c22f9cec9
Subbotina Eugenia <63358524+jenyasubbotina@users.noreply.github.com>
2020-06-07 21:07:14 +0500

    Add files via upload

--- PasTelegramBotAPI.pas
+++ PasTelegramBotAPI.pas
@@ -8,35 +8,56 @@ uses
 const
     telegramUrl = 'https://api.telegram.org/bot';
 
+var
+    off: string := '1';
+
 type
     TBot = class
     private 
         TBotToken: string;
     public 
         constructor Create(token: string);
-        function GetUpdates(offset: integer := 0; limit: integer := 10; timeout: integer := 0): string;
+        function GetUpdates(offset : string := '0'; limit: integer := 10; timeout: integer := 0): string;
         function SendMessage(chatID: string; message: string): string;
+        function SendPhoto(chatID: string; photo: string): string;
+    end;
+
+type
+    Json = class
+    private 
+        jtstring: string;
+    public 
+        function Parse(jstr: string): Dictionary<string, string>;
     end;
 
 implementation
+
+function Json.Parse(jstr: string): Dictionary<string, string>;
+begin
+    
+end;
 
 constructor TBot.Create(token: string);
 begin
     TBotToken := Token;
 end;
 
-function TBot.GetUpdates(offset: integer; limit: integer; timeout: integer): string;
+function TBot.GetUpdates(offset: string; limit: integer; timeout: integer): string;
 var
     request: HttpWebRequest;
     readstream: StreamReader;
     res: string;
 begin
     ServicePointManager.SecurityProtocol := SecurityProtocolType.Tls12 or SecurityProtocolType.Tls11 or SecurityProtocolType.Tls;
-    request := HttpWebRequest(WebRequest.Create(telegramUrl + TBotToken + '/getUpdates'));
+    if (offset = '0') and (off <> '') then 
+        offset := off;
+    request := HttpWebRequest(WebRequest.Create(telegramUrl + TBotToken + '/getUpdates' + '?offset=' + (offset.ToBigInteger+1).ToString));
     request.Method := 'GET';
     readstream := new StreamReader(request.GetResponse.GetResponseStream, System.Text.Encoding.UTF8);
     res := readstream.ReadToEnd;
     readstream.Close;
+    if (res.IndexOf('update_id') <> -1) then
+        off := res.Substring(res.IndexOf('update_id')+11, res.IndexOf(',')-1);
     Result := res;
 end;
 
@@ -60,6 +81,11 @@ begin
     var responseFromServer := reader.ReadToEnd();
     writeln(responsefromserver);
     response.Close();
+end;
+
+function TBot.SendPhoto(chatID: string; photo: string): string;
+begin
+    
 end;
 
 end.
